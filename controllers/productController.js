282d98717d734d071364cbe9910fb8bd2918a527
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2024-12-02 02:03:04 +0530

    create product file

---
+++ controllers/productController.js
@@ -0,0 +1,38 @@
+import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
+
+export async function createProduct(req, res) {
+
+    if(!isAdmin(req)) {
+        res.json({
+            message: "Please login as administrator to add products"
+        });
+        return;
+    }
+
+    const newProductData = req.body;
+
+    try {
+        const product = new Product(newProductData);
+        await product.save();
+        res.json({
+            message: "Product created"
+        });
+    } catch (error) {
+        res.json({
+            message: error
+        });
+    }
+}
+
+export async function getProducts(req, res) {
+
+    try {
+        const products = await Product.find({});
+        res.json(products);
+    } catch (error) {
+        res.json({
+            message: error
+        });
+    }
+}
